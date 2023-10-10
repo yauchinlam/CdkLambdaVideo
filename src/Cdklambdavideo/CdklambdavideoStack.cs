57400fbc82cc8c7f0f52451e7ce82ba3d72733a9
Yauchin M. Lam <yauchinlam17@gmail.com>
2023-10-09 21:18:32 -0400

    Initial commit

---
+++ src/Cdklambdavideo/CdklambdavideoStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace Cdklambdavideo
+{
+    public class CdklambdavideoStack : Stack
+    {
+        internal CdklambdavideoStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
